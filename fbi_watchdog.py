f4a591adbe5fb3c9457029397b3db8952ef0a2ca
LemonySnicket <bendriven88@gmail.com>
2026-06-05 12:44:32 -0700

    Update fbi_watchdog.py

--- fbi_watchdog.py
+++ fbi_watchdog.py
@@ -336,7 +336,7 @@ class SiteManager:
 site_manager = SiteManager()
 DOMAINS = site_manager.domains
 ONION_SITES = site_manager.onion_sites
-list_sites(ONION_SITES, "Onion Sites", "magenta", DOMAINS)
+_list_sites(ONION_SITES, "Onion Sites", "magenta", DOMAINS)
 
 class DWIConfig:
     def __init__(self):
