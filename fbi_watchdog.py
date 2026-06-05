bfe95089ba72873831bb562579bc010cf673cca9
LemonySnicket <bendriven88@gmail.com>
2026-06-04 17:41:33 -0700

    Update fbi_watchdog.py

--- fbi_watchdog.py
+++ fbi_watchdog.py
@@ -335,7 +335,6 @@ class SiteManager:
 
 site_manager = SiteManager()
 DOMAINS = site_manager.domains
-
 ONION_SITES = site_manager.onion_sites
 list_sites(ONION_SITES, "Onion Sites", "magenta", DOMAINS)
 
