cf56857c1b4612c4af382ecf518d85b7ca60ecb1
LemonySnicket <bendriven88@gmail.com>
2026-06-05 13:07:43 -0700

    Update fbi_watchdog.py

--- fbi_watchdog.py
+++ fbi_watchdog.py
@@ -3547,7 +3547,7 @@ def _add_site_prompt():
     
 # --- Setup ---
 site_manager = SiteManager()
-DOMAINS = DOMAINS, ONION_SITES site_manager.domains
+DOMAINS = site_manager.domains
 ONION_SITES = site_manager.onion_sites
 
 # REMOVE this line:
